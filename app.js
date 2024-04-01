5df95ac25611d35a1996e39f44ac411f43dfc9d3
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-04-01 23:41:19 +0900

    lesson1 learning

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("ここにコードを記述します。");
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if(resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
+}
+var combineAges = combine(30, 26, 'as number');
+console.log(combineAges);
+var combineStringAges = combine('30', '26', 'as number');
+console.log(combineAges);
+var combineNames = combine('Max', 'Anna', 'as text');
+console.log(combineNames);
