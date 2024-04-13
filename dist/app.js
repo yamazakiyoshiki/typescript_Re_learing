75a374711e89826d9fa697a76a23c83755f7cb7c
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-04-14 00:36:57 +0900

    class learning

--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,126 @@
 "use strict";
-const buttons = document.querySelector('button');
-if (buttons) {
-    buttons.addEventListener('click', event => {
-        console.log(event);
-    });
+class Department {
+    static createEmployee(name) {
+        return { name: name };
+    }
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+        console.log(Department.fiscalYear);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
-const resultHobby = activeHobbies.push(...hobbies);
-console.log(activeHobbies);
-console.log(resultHobby);
-const persons = {
-    name: 'Max',
-    age: 30,
-};
-const copiedPerson = Object.assign(Object.assign({}, persons), { pref: 'tyokyo' });
-console.log(persons);
-console.log(copiedPerson);
-const adds = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-const addedNumbers = adds(5, 10, 2);
-console.log(addedNumbers);
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
+Department.fiscalYear = 2020;
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+    describe() {
+        console.log("IT部門 - ID" + this.id);
+    }
+}
+class AccountingDepartment extends Department {
+    describe() {
+        console.log("会計部門 - ID" + this.id);
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('レポートが見つかりません。');
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('正しい値を設定してください。');
+        }
+        this.addReport(value);
+    }
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance;
+    }
+    addReport(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+}
+const employee1 = Department.createEmployee('Yoshiki');
+console.log(employee1, Department.fiscalYear);
+const it = new ITDepartment('d1', ['Max']);
+it.addEmployee('Max');
+it.addEmployee('Manu');
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
+const accounting = AccountingDepartment.getInstance();
+accounting.mostRecentReport = '通気会計レポート';
+accounting.addReport('Something');
+console.log(accounting.mostRecentReport);
+accounting.addEmployee('Max');
+accounting.addEmployee('Mani');
+accounting.describe();
 //# sourceMappingURL=app.js.map
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.hello = function() {
+        console.log('hello' + this.name);
+    }
+}
+
+Person.prototype.hello = function() {
+    console.log('hello' + this.name);
+}
+
+const bob = new Person('Bob', 25);
+
+bob.hello();
+console.log(bob);
+
+function F(a, b) {
+    this.a = a;
+    this.b = b;
+    return {};
+}
+
+F.prototype.c = function() {}
+
+function newOpe(C, ...args) {
+    const _this = Object.create(C.prototype);
+    const result = C.apply(_this, args);
+    // console.log(result, _this);
+    if(typeof result == "object" && result !== null) {
+        return result;
+    }
+
+    return _this;
+}
+
+const instance = newOpe(F, 1, 2);
+console.log(instance);
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,138 @@
-// const adds = (a: number = 3, b: number = 1) => a + b;
+abstract class Department {
+  static fiscalYear = 2020;
+  // private readonly id: string;
+  // name: string;
+  protected employees: string[] = [];
 
-// const printOutput: (output: string | number) => void = output => {
-//   console.log(output);
-// };
+  static createEmployee(name: string) {
+    return {name: name};
+  }
 
-// printOutput(adds());
+  constructor(protected readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+    console.log(Department.fiscalYear);
+  }
 
-const buttons = document.querySelector('button');
+  abstract describe(this: Department): void;
 
-if (buttons) {
-  buttons.addEventListener('click', event => {
-    console.log(event);
-  });
+  addEmployee(employee: string) {
+
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
 
-const resultHobby = activeHobbies.push(...hobbies);
-console.log(activeHobbies);
-console.log(resultHobby);
+  describe() {
+    console.log("IT部門 - ID" + this.id);
+  }
+}
 
-const persons = {
-  name: 'Max',
-  age: 30,
-};
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
 
-const copiedPerson = {...persons, pref: 'tyokyo'};
-console.log(persons);
-console.log(copiedPerson);
+  describe() {
+    console.log("会計部門 - ID" + this.id);
+  }
 
-const adds = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
+  get mostRecentReport() {
+    if(this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('レポートが見つかりません。');
+  }
 
-const addedNumbers = adds(5, 10, 2);
-console.log(addedNumbers);
+  set mostRecentReport(value: string) {
+    if(!value) {
+      throw new Error('正しい値を設定してください。')
+    }
+    this.addReport(value);
+  }
 
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
+  private constructor(id: string,private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if(AccountingDepartment.instance) {
+      return this.instance
+    }
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+}
+
+const employee1 = Department.createEmployee('Yoshiki');
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDepartment('d1', ['Max']);
+
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
+
+it.describe();
+it.printEmployeeInformation();
+
+console.log(it);
+
+// const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+
+accounting.mostRecentReport = '通気会計レポート';
+accounting.addReport('Something');
+console.log(accounting.mostRecentReport);
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Mani');
+
+// accounting.printReports();
+// accounting.printEmployeeInformation();
+accounting.describe();
+
+// const accountingCopy = {
+//   name: "DUMMY",
+//   describe: accounting.describe,
+// };
+
+// accountingCopy.describe();
+
+
+
+
+// class Ucl {
+//   constructor(id: string, name: string[], leage: string[]) {}
+
+//   addCheam(chaem: string) {
+
+//   }
+// }
