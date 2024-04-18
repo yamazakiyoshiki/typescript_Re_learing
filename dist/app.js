95acf1528c4f3faf79a97e35ff34bcb2cd713e19
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-04-19 00:54:09 +0900

    decorator comp

--- dist/app.js
+++ dist/app.js
@@ -1,52 +1,178 @@
 "use strict";
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+function Logger(logString) {
+    console.log("logger ファクトリ");
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 20 });
-const mergedObj2 = merge({ name: 'Max' }, { age: 30 });
-console.log(mergedObj.age);
-function countAndDescribe(element) {
-    let descriptionText = '値がありません。';
-    if (element.length > 0) {
-        descriptionText = '値は' + element.length + '個です。';
-    }
-    return [element, descriptionText];
+function WithTemplate(template, hookId) {
+    console.log("template ファクトリ");
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log("テンプレートを表示");
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent = this.name;
+                }
+            }
+        };
+    };
 }
-console.log(countAndDescribe(['Sports', 'Cooking']));
-function extractAndConvert(obj, key) {
-    return 'Value' + obj[key];
-}
-const result4 = extractAndConvert({ name: 'Max' }, "name");
-console.log(result4);
-class DataStorage {
+let Person2 = class Person2 {
     constructor() {
-        this.data = [];
+        this.name = 'Max';
+        console.log("Personオブジェクトを作成中...");
     }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
+};
+Person2 = __decorate([
+    Logger("ログ出力中"),
+    WithTemplate("<h1>Personオブジェクト</h1>", "app")
+], Person2);
+const pers = new Person2();
+console.log(pers);
+function Log(target, propertyName) {
+    console.log("Property デコレータ");
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("Accessor デコレータ");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log("Method デコレータ");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log("Parameter デコレータ");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
         }
-        this.data.splice(this.data.indexOf(item), 1);
+        else {
+            throw new Error("不正な価格です - 0以下は設定できません");
+        }
     }
-    getItems() {
-        return [...this.data];
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
     }
 }
-const textStorage = new DataStorage();
-textStorage.addItem("Data1");
-textStorage.addItem("Data2");
-textStorage.removeItem("Data1");
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-function createCourseGoal(title, description, date) {
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
+const p1 = new Product("Book", 100);
+const p2 = new Product("Book", 200);
+function AutoBind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
 }
-const names = ['Max', 'Anna'];
+class Printer {
+    constructor() {
+        this.message = 'クリックしました！';
+    }
+    showMessage() {
+        console.log(this.message);
+    }
+}
+__decorate([
+    AutoBind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+p.showMessage();
+const button2 = document.querySelector("button");
+button2.addEventListener('click', p.showMessage);
+const registeredValidators = {};
+function Required(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ['required'] });
+}
+function PositiveNumber(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ['positive'] });
+}
+function validate(obj) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        console.log(prop);
+        for (const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+class Course {
+    constructor(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+}
+__decorate([
+    Required
+], Course.prototype, "title", void 0);
+__decorate([
+    PositiveNumber
+], Course.prototype, "price", void 0);
+const courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title');
+    const priceEl = document.getElementById('price');
+    const title = titleEl.value;
+    const price = +priceEl.value;
+    const createdCourse = new Course(title, price);
+    if (!validate(createdCourse)) {
+        alert('正しく入力してください!');
+        return;
+    }
+    console.log(createdCourse);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,98 +1,197 @@
-// export{}
-
-// const names: Array<string> = [];
-// names[0].split('');
-
-// const promise: Promise<number> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(10)
-//   }, 2000);
-// })
-
-// promise.then(data => {
-//   data.to('');
-// })
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
-}
-
-const mergedObj = merge({name: 'Max', hobbies: ['Sports']},
-{age: 20});
-const mergedObj2 = merge({name: 'Max'}, {age: 30});
-console.log(mergedObj.age);
-
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = '値がありません。';
-  if(element.length > 0) {
-    descriptionText = '値は' + element.length + '個です。';
-  }
-  return [element, descriptionText]
-}
-
-console.log(countAndDescribe(['Sports', 'Cooking']));
-
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-  return 'Value' + obj[key];
-}
-
-const result4 = extractAndConvert({name: 'Max'}, "name");
-console.log(result4);
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item)
-  }
-
-  removeItem(item: T) {
-    if(this.data.indexOf(item) === -1) {
-      return;
-    }
-    this.data.splice(this.data.indexOf(item), 1);// -1
-  }
-
-  getItems() {
-    return [...this.data];
+function Logger(logString: string) {
+  console.log("logger ファクトリ");
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem("Data1");
-textStorage.addItem("Data2");
-textStorage.removeItem("Data1");
-console.log(textStorage.getItems());
+function WithTemplate(template: string, hookId: string) {
+  console.log("template ファクトリ");
+  return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("テンプレートを表示");
+        const hookEl = document.getElementById(hookId);
+        if(hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
 
-const numberStorage = new DataStorage<number>();
+@Logger("ログ出力中")
+@WithTemplate("<h1>Personオブジェクト</h1>", "app")
+class Person2 {
+  name = 'Max';
 
-// const objStorage = new DataStorage<object>();
-// const obj4 = {name: 'Max'};
-// objStorage.addItem(obj4);
-// objStorage.addItem({name: 'Manu'});
-// //...
-// objStorage.removeItem(obj4);
-// console.log(objStorage.getItems());
+  constructor() {
+    console.log("Personオブジェクトを作成中...");
+  }
+}
 
-interface CourseGoal {
+const pers = new Person2();
+
+console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property デコレータ");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log("Method デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
   title: string;
-  description: string;
-  completeUntil: Date;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if(val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("不正な価格です - 0以下は設定できません")
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+@Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
 }
 
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
+const p1 = new Product("Book", 100);
+const p2 = new Product("Book", 200);
+
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
 }
 
-const names: Readonly<string[]> = ['Max', 'Anna'];
+class Printer {
+  message = 'クリックしました！'
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+p.showMessage();
+
+const button2 = document.querySelector("button")!;
+button2.addEventListener('click', p.showMessage);
+
+//---
+
+interface ValidatorConfig {
+  [prop: string]: {
+    [validatableProp: string]: string[] //['required', 'positive']
+  }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required'],
+  }
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive'],
+}}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name]
+  if(!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for(const prop in objValidatorConfig) {
+    console.log(prop);
+    for(const validator of objValidatorConfig[prop]) {
+      switch(validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if(!validate(createdCourse)) {
+    alert('正しく入力してください!')
+    return;
+  }
+  console.log(createdCourse);
+});
